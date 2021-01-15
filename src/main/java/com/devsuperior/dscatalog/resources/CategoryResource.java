602f37c13c75985a628c2861cdb5c86d7fcd13a9
Wesley de Andrade Santos <wesleyandrade298@gmail.com>
2021-01-14 22:22:18 -0300

    DTO

--- src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -22,8 +22,8 @@ public class CategoryResource {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = categoryService.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = categoryService.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -14,8 +17,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	public List<Category> findAll(){
-		return categoryRepository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list = categoryRepository.findAll();
+
+		return list.stream()
+				.map(x -> new CategoryDTO(x))
+				.collect(Collectors.toList());
+		
+		
+		
 	}
 
 }
