73c2bf53ff87ae7fad36f22e8aa4b2012ae2e2f0
Wesley de Andrade Santos <wesleyandrade298@gmail.com>
2021-01-16 20:43:51 -0300

    Delete Category

---
+++ src/main/java/com/devsuperior/dscatalog/services/exception/DataBaseException.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog.services.exception;
+
+public class DataBaseException extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
