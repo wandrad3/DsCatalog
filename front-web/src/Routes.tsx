78dc52f441e0eeff4acf468b8ef948923a10c9eb
Wesley de Andrade Santos <wesleyandrade298@gmail.com>
2021-03-17 23:19:26 -0300

    Add base admin from structure

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from './pages/Home'
 import Catalog from './pages/Catalog'
 import Admin from './pages/Admin';
@@ -22,7 +22,9 @@ const Routes = () => {
                 <ProductDetails />
 
             </Route>
+            <Redirect from="/admin" to="/admin/products" exact />
             <Route path="/admin">
+                
                 <Admin />
             </Route>
 
--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Route,Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
-
+import Products from './components/Products'
+import './styles.scss'
 const Admin = () =>{
     return(
 
-        <div>
+        <div className="admin-container">
             <Navbar/>
+            <div className="admin-content">
+                    <Switch>
+                        <Route path="/admin/products">
+                           <Products/>
+
+                        </Route>
+                        <Route path="/admin/categories">
+                            <h1>Categorias</h1>
+
+                        </Route>
+                        <Route path="/admin/users">
+                            <h1>Usuários</h1>
+
+                        </Route>
+                    </Switch>
+            </div>
         </div>
 
     )
